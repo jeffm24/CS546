1cc72cf6c9221e81edfb31afb4b7f7f4c28a30ab
jeffm24 <jmariconda24@gmail.com>
2016-02-09 01:56:18 -0500

    finished dates.js; TODO: finish test.js

--- assignment1/dates.js
+++ assignment1/dates.js
@@ -2,3 +2,77 @@
  * Jeff Mariconda
  * CS-546
  */
+
+/*
+ *  Return the number of days between the current date and someDate.
+ */
+exports.daysUntil = function (someDate)
+{
+    var today = new Date();
+
+    if (someDate == null) {
+        throw "ERROR: null date";
+    } else if (someDate < today) {
+        throw "ERROR: date falls before current date";
+    }
+
+    var msPerDay = 1000 * 60 * 60 * 24;
+    var numDays = Math.ceil((someDate - today) / msPerDay);
+
+    return numDays;
+}
+
+console.log(exports.daysUntil(new Date(2020, 11, 17)));
+//console.log(exports.daysUntil(null));
+
+/*
+ *  Return the number of days left in the year.
+ */
+exports.daysLeftInYear = function ()
+{
+    var today = new Date();
+
+    return exports.daysUntil(new Date(today.getFullYear(), 11, 31));
+}
+
+console.log(exports.daysLeftInYear());
+
+/*
+ *  Return the number of days that have passed since someDate.
+ */
+exports.daysSince = function (someDate)
+{
+    var today = new Date();
+
+    if (someDate == null)
+        throw "ERROR: null date";
+    else if (someDate > today)
+        throw "ERROR: date falls after current date"
+
+    var msPerDay = 1000 * 60 * 60 * 24;
+    var numDays = Math.floor((today - someDate) / msPerDay);
+
+    return numDays;
+}
+
+console.log(exports.daysSince(new Date(2014, 11, 17)));
+
+/*
+ *  Return the date that is both a Friday and the 13th.
+ */
+exports.nextFridayTheThirteenth = function ()
+{
+    var today = new Date();
+
+    while (true) {
+        if (today.getDay() == 0 && today.getDate() == 1) {
+            today.setDate(13);
+            return today;
+        } else {
+            today.setMonth((today.getMonth() + 1) % 12);
+            today.setDate(1);
+        }
+    }
+}
+
+console.log(exports.nextFridayTheThirteenth());
